5257eb3ca1cc8c32172451cb8564e7002dc57b17
Dmitry Polyakov <d.poliakov@bpm-cons.ru>
2022-04-25 14:29:37 +0400

    1-3 использован роутер chi

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,12 +4,13 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
+	"github.com/akaletr/yandex/internal/storage"
 	"io"
 	"net/http"
 	"net/url"
-	"strings"
 
-	"github.com/akaletr/yandex/internal/storage"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type App interface {
@@ -18,56 +19,61 @@ type App interface {
 
 type server struct {
 	storage storage.Storage
+	path    string
 }
 
-func NewServer() (App, error) {
+func NewServer() (*server, error) {
 	return &server{
 		storage: storage.NewStorage(),
+		path:    "http://localhost",
 	}, nil
 }
 
 func (app *server) Start() error {
-	http.HandleFunc("/", app.handler)
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Post("/", app.addLink)
+	router.Get("/{id}", app.getLink)
+
 	return http.ListenAndServe(":8080", nil)
 }
 
-func (app *server) handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodPost:
-		w.WriteHeader(http.StatusCreated)
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		hasher := sha1.New()
-		_, err = hasher.Write(body)
-
-		linkID := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
-
-		err = app.storage.Write(linkID, string(body))
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		link := url.URL{
-			Scheme: "http",
-			Host:   r.Host,
-			Path:   linkID,
-		}
-
-		_, _ = w.Write([]byte(link.String()))
-	case http.MethodGet:
-		link, err := app.storage.Read(strings.TrimPrefix(r.URL.Path, "/"))
-
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			_, _ = w.Write([]byte(""))
-		}
-		w.Header().Set("Location", link)
-		w.WriteHeader(http.StatusTemporaryRedirect)
-	default:
-		w.WriteHeader(http.StatusNotFound)
+func (app *server) addLink(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
 	}
 
+	hasher := sha1.New()
+	_, err = hasher.Write(body)
+
+	linkID := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+
+	err = app.storage.Write(linkID, string(body))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	link := url.URL{
+		Scheme: "http",
+		Host:   r.Host,
+		Path:   linkID,
+	}
+
+	_, _ = w.Write([]byte(link.String()))
+}
+
+func (app *server) getLink(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	link, err := app.storage.Read(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		_, _ = w.Write([]byte(""))
+	}
+
+	w.Header().Set("Location", link)
+	w.WriteHeader(http.StatusTemporaryRedirect)
 }
